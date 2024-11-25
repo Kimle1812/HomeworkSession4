26e343b886247202e7c401b0b1d8143cfd378b0f
Kimle1812 <nguyenthikimle18122003@gmail.com>
2024-11-25 08:29:20 +0700

    Update Homework4.9.cpp

--- Homework4.9.cpp
+++ Homework4.9.cpp
@@ -9,14 +9,14 @@ int main(){
 	scanf("%d", &year);
 	switch(month){
 		case 1 : case 5 : case 3 : case 7 : case 8 : case 10 : case 12 :
-		if(day == 31 ){
+		if(0 < day <= 31 ){
 			printf("Hop le"); 
 		}else {
 			printf("Khong hop le"); 
 		} 
 		break;
 		case 4 : case 6 : case 9 : case 11 :
-		if(day == 30){
+		if(0 < day <=  30){
 			printf("Hop le"); 
 		} else {
 			printf("Khong hop le"); 
@@ -24,13 +24,13 @@ int main(){
 		break;
 		case 2 :
 		if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
-			if(day == 28){
+			if(0 < day <= 28){
 				printf("Hop le"); 
 			} else {
 				printf("Khong hop le"); 
 			} 
 		} else {
-			if(day == 29){
+			if(0 < day <= 29){
 				printf("Hop le"); 
 			} else {
 				printf("Khong hop le"); 
